621ee36e6689de0a91b3705901233761c1caba62
Jonathan Bradshaw <jb@nrgup.net>
2020-12-14 23:12:18 -0500

    Linting fixes

--- Drivers/DavisWeatherLinkLive.groovy
+++ Drivers/DavisWeatherLinkLive.groovy
@@ -23,47 +23,87 @@
   *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
   *  SOFTWARE.
  */
-static final String version() { "0.1" }
+import hubitat.scheduling.AsyncResponse
 
 metadata {
-    definition (name: "Davis WeatherLink Live", namespace: "jbradshaw", author: "Jonathan Bradshaw", importUrl: "https://raw.githubusercontent.com/bradsjm/hubitat/master/DavisWeatherLinkLive.groovy") {
-        capability "Polling"
-        capability "Relative Humidity Measurement"
-        capability "Pressure Measurement"
-        capability "Refresh"
-        capability "Sensor"
-        capability "Temperature Measurement"
- 		capability "Ultraviolet Index"
+    definition (
+        name: 'Davis WeatherLink Live',
+        namespace: 'nrgup',
+        author: 'Jonathan Bradshaw'
+    ) {
+        capability 'Polling'
+        capability 'Relative Humidity Measurement'
+        capability 'Pressure Measurement'
+        capability 'Sensor'
+        capability 'Temperature Measurement'
+        capability 'Ultraviolet Index'
 
         // Wind
-        attribute "windDirection", "number"
-        attribute "windSpeed", "number"
-        attribute "windGust", "number"
-        // Rain
-        attribute "raining", "boolean"
-        attribute "rainRate", "number"
-        attribute "rainDaily", "number"
-        attribute "rain24h", "number"
-    }
+        attribute 'windDirection', 'number'
+        attribute 'windSpeed', 'number'
+        attribute 'windGust', 'number'
 
-    preferences() {
-        input "pollInterval", "enum", title: "Polling Interval", required: true, defaultValue: "1 Minute", options: [1:"1 Minute", 5:"5 Minutes", 10:"10 Minutes", 15:"15 Minutes", 30:"30 Minutes", 60:"60 Minutes"]
-        input "weatherLinkHost", "string", required: true, title: "WeatherLink Live IP Address"
-        input "weatherLinkPort", "number", required: true, title: "WeatherLink Live Port Number", defaultValue: 80
-        input "transmitterId", "number", required: true, title: "Davis Transmitter ID", defaultValue: 1
-        input name: "logEnable", type: "bool", title: "Enable Debug logging", description: "Automatically disabled after 30 minutes", required: true, defaultValue: true
+        // Rain
+        attribute 'raining', 'boolean'
+        attribute 'rainRate', 'number'
+        attribute 'rainDaily', 'number'
+        attribute 'rain24h', 'number'
+
+        preferences {
+            section {
+                input name: 'weatherLinkHost',
+                      title: 'WeatherLink Live IP Address',
+                      type: 'string',
+                      required: true
+
+                input name: 'weatherLinkPort',
+                      title: 'WeatherLink Live Port Number',
+                      type: 'number',
+                      required: true,
+                      defaultValue: 80
+            }
+
+            section {
+                input name: 'transmitterId',
+                      title: 'Davis Transmitter ID',
+                      type: 'number',
+                      required: true,
+                      defaultValue: 1
+
+                input name: 'pollInterval',
+                      title: 'Polling Interval',
+                      type: 'enum',
+                      required: true,
+                      defaultValue: 1,
+                      options: [
+                        1: '1 Minute',
+                        5: '5 Minutes',
+                        10: '10 Minutes',
+                        15: '15 Minutes',
+                        30: '30 Minutes',
+                        60: '60 Minutes'
+                    ]
+
+                input name: 'logEnable',
+                      title: 'Enable Debug logging',
+                      type: 'bool',
+                      description: 'Automatically disabled after 30 minutes',
+                      required: false,
+                      defaultValue: true
+            }
+        }
     }
 }
 
 // Called when the device is first created.
 void installed() {
-    log.info "${device.displayName} driver v${version()} installed"
+    log.info "${device.displayName} driver installed"
 }
 
 // Called when the device is removed.
 void uninstalled() {
     mqttDisconnect()
-    log.info "${device.displayName} driver v${version()} uninstalled"
+    log.info "${device.displayName} driver uninstalled"
 }
 
 // Called when the preferences of a device are updated.
@@ -71,106 +111,138 @@ void updated() {
     unschedule()
 
     if (settings.pollInterval > 0) {
-    	int randomSeconds = new Random(now()).nextInt(60)
+        int randomSeconds = new Random(now()).nextInt(60)
         String sched = "${randomSeconds} 0/${pollInterval} * * * ?"
-        schedule("${sched}", "poll")
+        schedule("${sched}", 'poll')
     }
 
-    if (logEnable) runIn(1800, "logsOff")
+    if (logEnable) { runIn(1800, 'logsOff') }
 
-    refresh()
-}
-
-// Called when the user requests a refresh (from Refresh capability)
-void refresh() {
     poll()
 }
 
 // Poll Weatherlink for latest conditions
 void poll() {
-    def params = [
+    Map params = [
         uri: "http://${weatherLinkHost}:${weatherLinkPort}",
-        path: "/v1/current_conditions",
-        requestContentType: "application/json"
+        path: '/v1/current_conditions',
+        requestContentType: 'application/json'
     ]
-    if (logEnable) log.trace "Requesting ${params.uri}${params.path}"
-    asynchttpGet("handler", params)
+    if (logEnable) { log.trace "Requesting ${params.uri}${params.path}" }
+    asynchttpGet('handler', params)
 }
 
-// Handle response from web request
-private void handler(response, data) {
-    def status = response.status
+private static String splitCamelCase(String s) {
+   return s.replaceAll(
+      String.format('%s|%s|%s',
+         '(?<=[A-Z])(?=[A-Z][a-z])',
+         '(?<=[^A-Z])(?=[A-Z])',
+         '(?<=[A-Za-z])(?=[^A-Za-z])'
+      ),
+      ' '
+   )
+}
+
+/* groovylint-disable-next-line UnusedPrivateMethod, UnusedPrivateMethodParameter */
+private void handler(AsyncResponse response, Object data) {
+    int status = response.status
     if (status == 200) {
-        if (logEnable) log.trace "WeatherLink returned: ${response.data}"
-        def json = response.json
+        if (logEnable) { log.trace "WeatherLink returned: ${response.data}" }
+        Map json = response.json
         if (json?.data) {
-    		parseWeatherData(json.data)
+            parseWeatherData(json.data)
         } else if (json?.error) {
             log.error "WeatherLink JSON parsing error: ${json.error.code} ${json.error.message}"
         }
-	} else {
-		log.error "WeatherLink returned HTTP status ${status}"
-	}
+    } else {
+        log.error "WeatherLink returned HTTP status ${status}"
+    }
 }
 
+/* groovylint-disable-next-line UnusedPrivateMethod */
 private void logsOff() {
     log.warn "${device.displayName} debug logging disabled"
-    device.updateSetting("logEnable", [value:"false", type:"bool"])
+    device.updateSetting('logEnable', [value:'false', type:'bool'])
 }
 
 private void parseWeatherData(Map json) {
+    List<Map> events = []
     int rainMultiplier = 0
-    String rainUnit = ""
+    String rainUnit = ''
 
     // Transfer data into state for debug
     state.lastResponse = json.conditions
 
-    json.conditions.each {
-        // Parse data structures
-        switch (it.data_structure_type) {
-        case 1:
-            if (it.txid != transmitterId) return
-            if (logEnable) log.debug "Received ISS #${it.txid} Current Conditions data"
-            switch (it.rain_size) {
-                case 1:
-                    rainMultiplier = 0.01
-                    rainUnit = "in"
-                    break
-                case 2:
-                    rainMultiplier = 0.2
-                    rainUnit = "mm"
-                    break
-                case 3:
-                    rainMultiplier = 0.1
-                    rainUnit = "mm"
-                    break
-                case 4:
-                    rainMultiplier = 0.001
-                    rainUnit = "in"
-                    break
-            }
-            sendEvent(name: "temperature", value: it.temp, unit: "F")
-            sendEvent(name: "humidity", value: it.hum, unit: "%")
-            sendEvent(name: "ultravioletIndex", value: it.uv_index, unit: 'uvi')
-            sendEvent(name: "windSpeed", value: it.wind_speed_avg_last_1_min, unit: "mph")
-            sendEvent(name: "windDirection", value: it.wind_dir_scalar_avg_last_1_min, unit: "degrees")
-            sendEvent(name: "windGust", value: it.wind_speed_hi_last_2_min, unit: "mph")
-            sendEvent(name: "raining", value: it.rainfall_last_15_min > 0 ? "true" : "false")
-            sendEvent(name: "rainRate", value: (it.rain_rate_hi * rainMultiplier), unit: rainUnit)
-            sendEvent(name: "rainDaily", value: (it.rainfall_daily * rainMultiplier), unit: rainUnit)
-            sendEvent(name: "rain24h", value: (it.rainfall_last_24_hr * rainMultiplier), unit: rainUnit)
-            break
-        case 2:
-            if (it.txid != transmitterId) return
-            if (logEnable) log.debug "[${device?.displayName}] Received Leaf/Soil Moisture sensor #${it.txid} data"
-            break
-        case 3:
-            if (logEnable) log.debug "[${device?.displayName}] Received Base Barometer data"
-            sendEvent(name: "pressure", value: it.bar_sea_level, unit: "inHg")
-            break
-        case 4:
-            if (logEnable) log.debug "[${device?.displayName}] Received Base Temperature/Humidity data"
-            break
+    json.conditions.each { c ->
+        switch (c.data_structure_type) {
+            case 1:
+                if (c.txid != transmitterId) { return }
+                if (logEnable) { log.debug "Received ISS #${c.txid} Current Conditions data" }
+                switch (c.rain_size) {
+                    case 1:
+                        rainMultiplier = 0.01
+                        rainUnit = 'in'
+                        break
+                    case 2:
+                        rainMultiplier = 0.2
+                        rainUnit = 'mm'
+                        break
+                    case 3:
+                        rainMultiplier = 0.1
+                        rainUnit = 'mm'
+                        break
+                    case 4:
+                        rainMultiplier = 0.001
+                        rainUnit = 'in'
+                        break
+                }
+                events << newEvent('temperature', c.temp, 'F')
+                events << newEvent('humidity', c.hum, '%')
+                events << newEvent('ultravioletIndex', c.uv_index, 'uvi')
+                events << newEvent('windSpeed', c.wind_speed_avg_last_1_min, 'mph')
+                events << newEvent('windDirection', c.wind_dir_scalar_avg_last_1_min, 'degrees')
+                events << newEvent('windGust', c.wind_speed_hi_last_2_min, 'mph')
+                events << newEvent('raining', c.rainfall_last_15_min > 0 ? 'true' : 'false')
+                events << newEvent('rainRate', c.rain_rate_hi * rainMultiplier, rainUnit)
+                events << newEvent('rainDaily', c.rainfall_daily * rainMultiplier, rainUnit)
+                events << newEvent('rain24h', c.rainfall_last_24_hr * rainMultiplier, rainUnit)
+                break
+            case 2:
+                if (c.txid != transmitterId) { return }
+                if (logEnable) {
+                    log.debug "[${device.displayName}] Received Leaf/Soil Moisture sensor #${c.txid} data"
+                }
+                break
+            case 3:
+                if (logEnable) { log.debug "[${device.displayName}] Received Base Barometer data" }
+                events << newEvent('pressure', c.bar_sea_level, 'inHg')
+                break
+            case 4:
+                if (logEnable) { log.debug "[${device.displayName}] Received Base Temperature/Humidity data" }
+                break
         }
     }
+
+    events.each { e ->
+        log.info e.descriptionText
+        if (device.currentValue(e.name) != e.value) {
+            sendEvent(e)
+        }
+    }
+}
+
+private Map newEvent(String name, Object value, String unit = null) {
+    String splitName = splitCamelCase(name).toLowerCase()
+    String description
+    if (device.currentValue(name) && value == device.currentValue(name)) {
+        description = "${device.displayName} ${splitName} is ${value}${unit ?: ''}"
+    } else {
+        description = "${device.displayName} ${splitName} was set to ${value}${unit ?: ''}"
+    }
+    return [
+        name: name,
+        value: value,
+        unit: unit,
+        descriptionText: settings.logTextEnable ? description : ''
+    ]
 }
