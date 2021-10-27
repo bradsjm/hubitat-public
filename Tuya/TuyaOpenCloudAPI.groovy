a9cd2a085cfa850887ed36040190259448919b8d
Jonathan Bradshaw <jb@nrgup.net>
2021-10-27 18:48:19 -0400

    Add country lookup

--- Tuya/TuyaOpenCloudAPI.groovy
+++ Tuya/TuyaOpenCloudAPI.groovy
@@ -46,7 +46,8 @@ import hubitat.scheduling.AsyncResponse
  *  10/14/21 - 0.1.6 - smart plug, vibration sensor; brightness and temperature sensors scaling bug fix
  *  10/17/21 - 0.1.7 - switched API to use device specification request to get both functions and status ranges
  *  10/25/21 - 0.1.8 - Added support for Window Shade using a custom component driver
- *  10/27/21 - 0.1.9 - Add support for heating devices with custom component driver
+ *  10/26/21 - 0.1.9 - Add support for heating devices with custom component driver
+ *  10/27/21 - 0.2.0 - Created country to datacenter map (https://developer.tuya.com/en/docs/iot/oem-app-data-center-distributed?id=Kafi0ku9l07qb)
  *
  *  Custom component drivers located at https://github.com/bradsjm/hubitat-drivers/tree/master/Component
  */
@@ -94,11 +95,12 @@ metadata {
                   title: 'Tuya Application Password',
                   required: true
 
-            input name: 'countryCode',
-                  type: 'number',
-                  title: 'Tuya Application Country Code',
+            input name: 'appCountry',
+                  title: 'Tuya Application Country',
+                  type: 'enum',
                   required: true,
-                  defaultValue: 1
+                  defaultValue: 'United States',
+                  options: tuyaCountries.country
 
             input name: 'logEnable',
                   type: 'bool',
@@ -479,7 +481,14 @@ void initialize() {
     state.clear()
     unschedule()
 
-    state.endPoint = 'https://openapi.tuyaus.com' // default US endpoint
+    Map datacenter = tuyaCountries.find { c -> c.country == settings.appCountry }
+    if (datacenter) {
+        state.endPoint = datacenter.endpoint
+        state.countryCode = datacenter.countryCode
+    } else {
+        log.error "${device.displayName} Country not set in configuration"
+    }
+
     state.tokenInfo = [ access_token: '', expire: now() ] // initialize token
     state.uuid = state?.uuid ?: UUID.randomUUID().toString()
 
@@ -548,6 +557,11 @@ void refresh() {
 void removeDevices() {
     log.info "${device.displayName} removing all child devices"
     childDevices.each { device -> deleteChildDevice(device.deviceNetworkId) }
+}
+
+// define country map
+private static Map country(String country, String countryCode, String endpoint = 'https://openapi.tuyaus.com') {
+    return [ country: country, countryCode: countryCode, endpoint: endpoint ]
 }
 
 /**
@@ -669,6 +683,249 @@ private static String translateColorName(Integer hue, Integer saturation) {
 
     return ''
 }
+
+@Field static final List<Map> tuyaCountries = [
+    country('Afghanistan', '93', 'https://openapi.tuyaeu.com'),
+    country('Albania', '355', 'https://openapi.tuyaeu.com'),
+    country('Algeria', '213', 'https://openapi.tuyaeu.com'),
+    country('American Samoa', '1-684', 'https://openapi.tuyaeu.com'),
+    country('Andorra', '376', 'https://openapi.tuyaeu.com'),
+    country('Angola', '244', 'https://openapi.tuyaeu.com'),
+    country('Anguilla', '1-264', 'https://openapi.tuyaeu.com'),
+    country('Antarctica', '672', 'https://openapi.tuyaus.com'),
+    country('Antigua and Barbuda', '1-268', 'https://openapi.tuyaeu.com'),
+    country('Argentina', '54', 'https://openapi.tuyaus.com'),
+    country('Armenia', '374', 'https://openapi.tuyaeu.com'),
+    country('Aruba', '297', 'https://openapi.tuyaeu.com'),
+    country('Australia', '61', 'https://openapi.tuyaeu.com'),
+    country('Austria', '43', 'https://openapi.tuyaeu.com'),
+    country('Azerbaijan', '994', 'https://openapi.tuyaeu.com'),
+    country('Bahamas', '1-242', 'https://openapi.tuyaeu.com'),
+    country('Bahrain', '973', 'https://openapi.tuyaeu.com'),
+    country('Bangladesh', '880', 'https://openapi.tuyaeu.com'),
+    country('Barbados', '1-246', 'https://openapi.tuyaeu.com'),
+    country('Belarus', '375', 'https://openapi.tuyaeu.com'),
+    country('Belgium', '32', 'https://openapi.tuyaeu.com'),
+    country('Belize', '501', 'https://openapi.tuyaeu.com'),
+    country('Benin', '229', 'https://openapi.tuyaeu.com'),
+    country('Bermuda', '1-441', 'https://openapi.tuyaeu.com'),
+    country('Bhutan', '975', 'https://openapi.tuyaeu.com'),
+    country('Bolivia', '591', 'https://openapi.tuyaus.com'),
+    country('Bosnia and Herzegovina', '387', 'https://openapi.tuyaeu.com'),
+    country('Botswana', '267', 'https://openapi.tuyaeu.com'),
+    country('Brazil', '55', 'https://openapi.tuyaus.com'),
+    country('British Indian Ocean Territory', '246', 'https://openapi.tuyaus.com'),
+    country('British Virgin Islands', '1-284', 'https://openapi.tuyaeu.com'),
+    country('Brunei', '673', 'https://openapi.tuyaeu.com'),
+    country('Bulgaria', '359', 'https://openapi.tuyaeu.com'),
+    country('Burkina Faso', '226', 'https://openapi.tuyaeu.com'),
+    country('Burundi', '257', 'https://openapi.tuyaeu.com'),
+    country('Cambodia', '855', 'https://openapi.tuyaeu.com'),
+    country('Cameroon', '237', 'https://openapi.tuyaeu.com'),
+    country('Canada', '1', 'https://openapi.tuyaus.com'),
+    country('Capo Verde', '238', 'https://openapi.tuyaeu.com'),
+    country('Cayman Islands', '1-345', 'https://openapi.tuyaeu.com'),
+    country('Central African Republic', '236', 'https://openapi.tuyaeu.com'),
+    country('Chad', '235', 'https://openapi.tuyaeu.com'),
+    country('Chile', '56', 'https://openapi.tuyaus.com'),
+    country('China', '86', 'https://openapi.tuyacn.com'),
+    country('Christmas Island', '61'),
+    country('Cocos Islands', '61'),
+    country('Colombia', '57', 'https://openapi.tuyaus.com'),
+    country('Comoros', '269', 'https://openapi.tuyaeu.com'),
+    country('Cook Islands', '682', 'https://openapi.tuyaus.com'),
+    country('Costa Rica', '506', 'https://openapi.tuyaeu.com'),
+    country('Croatia', '385', 'https://openapi.tuyaeu.com'),
+    country('Cuba', '53'),
+    country('Curacao', '599', 'https://openapi.tuyaus.com'),
+    country('Cyprus', '357', 'https://openapi.tuyaeu.com'),
+    country('Czech Republic', '420', 'https://openapi.tuyaeu.com'),
+    country('Democratic Republic of the Congo', '243', 'https://openapi.tuyaeu.com'),
+    country('Denmark', '45', 'https://openapi.tuyaeu.com'),
+    country('Djibouti', '253', 'https://openapi.tuyaeu.com'),
+    country('Dominica', '1-767', 'https://openapi.tuyaeu.com'),
+    country('Dominican Republic', '1-809', 'https://openapi.tuyaus.com'),
+    country('East Timor', '670', 'https://openapi.tuyaus.com'),
+    country('Ecuador', '593', 'https://openapi.tuyaus.com'),
+    country('Egypt', '20', 'https://openapi.tuyaeu.com'),
+    country('El Salvador', '503', 'https://openapi.tuyaeu.com'),
+    country('Equatorial Guinea', '240', 'https://openapi.tuyaeu.com'),
+    country('Eritrea', '291', 'https://openapi.tuyaeu.com'),
+    country('Estonia', '372', 'https://openapi.tuyaeu.com'),
+    country('Ethiopia', '251', 'https://openapi.tuyaeu.com'),
+    country('Falkland Islands', '500', 'https://openapi.tuyaus.com'),
+    country('Faroe Islands', '298', 'https://openapi.tuyaeu.com'),
+    country('Fiji', '679', 'https://openapi.tuyaeu.com'),
+    country('Finland', '358', 'https://openapi.tuyaeu.com'),
+    country('France', '33', 'https://openapi.tuyaeu.com'),
+    country('French Polynesia', '689', 'https://openapi.tuyaeu.com'),
+    country('Gabon', '241', 'https://openapi.tuyaeu.com'),
+    country('Gambia', '220', 'https://openapi.tuyaeu.com'),
+    country('Georgia', '995', 'https://openapi.tuyaeu.com'),
+    country('Germany', '49', 'https://openapi.tuyaeu.com'),
+    country('Ghana', '233', 'https://openapi.tuyaeu.com'),
+    country('Gibraltar', '350', 'https://openapi.tuyaeu.com'),
+    country('Greece', '30', 'https://openapi.tuyaeu.com'),
+    country('Greenland', '299', 'https://openapi.tuyaeu.com'),
+    country('Grenada', '1-473', 'https://openapi.tuyaeu.com'),
+    country('Guam', '1-671', 'https://openapi.tuyaeu.com'),
+    country('Guatemala', '502', 'https://openapi.tuyaus.com'),
+    country('Guernsey', '44-1481'),
+    country('Guinea', '224'),
+    country('Guinea-Bissau', '245', 'https://openapi.tuyaus.com'),
+    country('Guyana', '592', 'https://openapi.tuyaeu.com'),
+    country('Haiti', '509', 'https://openapi.tuyaeu.com'),
+    country('Honduras', '504', 'https://openapi.tuyaeu.com'),
+    country('Hong Kong', '852', 'https://openapi.tuyaus.com'),
+    country('Hungary', '36', 'https://openapi.tuyaeu.com'),
+    country('Iceland', '354', 'https://openapi.tuyaeu.com'),
+    country('India', '91', 'https://openapi.tuyain.com'),
+    country('Indonesia', '62', 'https://openapi.tuyaus.com'),
+    country('Iran', '98'),
+    country('Iraq', '964', 'https://openapi.tuyaeu.com'),
+    country('Ireland', '353', 'https://openapi.tuyaeu.com'),
+    country('Isle of Man', '44-1624'),
+    country('Israel', '972', 'https://openapi.tuyaeu.com'),
+    country('Italy', '39', 'https://openapi.tuyaeu.com'),
+    country('Ivory Coast', '225', 'https://openapi.tuyaeu.com'),
+    country('Jamaica', '1-876', 'https://openapi.tuyaeu.com'),
+    country('Japan', '81', 'https://openapi.tuyaus.com'),
+    country('Jersey', '44-1534'),
+    country('Jordan', '962', 'https://openapi.tuyaeu.com'),
+    country('Kazakhstan', '7', 'https://openapi.tuyaeu.com'),
+    country('Kenya', '254', 'https://openapi.tuyaeu.com'),
+    country('Kiribati', '686', 'https://openapi.tuyaus.com'),
+    country('Kosovo', '383'),
+    country('Kuwait', '965', 'https://openapi.tuyaeu.com'),
+    country('Kyrgyzstan', '996', 'https://openapi.tuyaeu.com'),
+    country('Laos', '856', 'https://openapi.tuyaeu.com'),
+    country('Latvia', '371', 'https://openapi.tuyaeu.com'),
+    country('Lebanon', '961', 'https://openapi.tuyaeu.com'),
+    country('Lesotho', '266', 'https://openapi.tuyaeu.com'),
+    country('Liberia', '231', 'https://openapi.tuyaeu.com'),
+    country('Libya', '218', 'https://openapi.tuyaeu.com'),
+    country('Liechtenstein', '423', 'https://openapi.tuyaeu.com'),
+    country('Lithuania', '370', 'https://openapi.tuyaeu.com'),
+    country('Luxembourg', '352', 'https://openapi.tuyaeu.com'),
+    country('Macao', '853', 'https://openapi.tuyaus.com'),
+    country('Macedonia', '389', 'https://openapi.tuyaeu.com'),
+    country('Madagascar', '261', 'https://openapi.tuyaeu.com'),
+    country('Malawi', '265', 'https://openapi.tuyaeu.com'),
+    country('Malaysia', '60', 'https://openapi.tuyaus.com'),
+    country('Maldives', '960', 'https://openapi.tuyaeu.com'),
+    country('Mali', '223', 'https://openapi.tuyaeu.com'),
+    country('Malta', '356', 'https://openapi.tuyaeu.com'),
+    country('Marshall Islands', '692', 'https://openapi.tuyaeu.com'),
+    country('Mauritania', '222', 'https://openapi.tuyaeu.com'),
+    country('Mauritius', '230', 'https://openapi.tuyaeu.com'),
+    country('Mayotte', '262', 'https://openapi.tuyaeu.com'),
+    country('Mexico', '52', 'https://openapi.tuyaus.com'),
+    country('Micronesia', '691', 'https://openapi.tuyaeu.com'),
+    country('Moldova', '373', 'https://openapi.tuyaeu.com'),
+    country('Monaco', '377', 'https://openapi.tuyaeu.com'),
+    country('Mongolia', '976', 'https://openapi.tuyaeu.com'),
+    country('Montenegro', '382', 'https://openapi.tuyaeu.com'),
+    country('Montserrat', '1-664', 'https://openapi.tuyaeu.com'),
+    country('Morocco', '212', 'https://openapi.tuyaeu.com'),
+    country('Mozambique', '258', 'https://openapi.tuyaeu.com'),
+    country('Myanmar', '95', 'https://openapi.tuyaus.com'),
+    country('Namibia', '264', 'https://openapi.tuyaeu.com'),
+    country('Nauru', '674', 'https://openapi.tuyaus.com'),
+    country('Nepal', '977', 'https://openapi.tuyaeu.com'),
+    country('Netherlands', '31', 'https://openapi.tuyaeu.com'),
+    country('Netherlands Antilles', '599'),
+    country('New Caledonia', '687', 'https://openapi.tuyaeu.com'),
+    country('New Zealand', '64', 'https://openapi.tuyaus.com'),
+    country('Nicaragua', '505', 'https://openapi.tuyaeu.com'),
+    country('Niger', '227', 'https://openapi.tuyaeu.com'),
+    country('Nigeria', '234', 'https://openapi.tuyaeu.com'),
+    country('Niue', '683', 'https://openapi.tuyaus.com'),
+    country('North Korea', '850'),
+    country('Northern Mariana Islands', '1-670', 'https://openapi.tuyaeu.com'),
+    country('Norway', '47', 'https://openapi.tuyaeu.com'),
+    country('Oman', '968', 'https://openapi.tuyaeu.com'),
+    country('Pakistan', '92', 'https://openapi.tuyaeu.com'),
+    country('Palau', '680', 'https://openapi.tuyaeu.com'),
+    country('Palestine', '970', 'https://openapi.tuyaus.com'),
+    country('Panama', '507', 'https://openapi.tuyaeu.com'),
+    country('Papua New Guinea', '675', 'https://openapi.tuyaus.com'),
+    country('Paraguay', '595', 'https://openapi.tuyaus.com'),
+    country('Peru', '51', 'https://openapi.tuyaus.com'),
+    country('Philippines', '63', 'https://openapi.tuyaus.com'),
+    country('Pitcairn', '64'),
+    country('Poland', '48', 'https://openapi.tuyaeu.com'),
+    country('Portugal', '351', 'https://openapi.tuyaeu.com'),
+    country('Puerto Rico', '1-787, 1-939', 'https://openapi.tuyaus.com'),
+    country('Qatar', '974', 'https://openapi.tuyaeu.com'),
+    country('Republic of the Congo', '242', 'https://openapi.tuyaeu.com'),
+    country('Reunion', '262', 'https://openapi.tuyaeu.com'),
+    country('Romania', '40', 'https://openapi.tuyaeu.com'),
+    country('Russia', '7', 'https://openapi.tuyaeu.com'),
+    country('Rwanda', '250', 'https://openapi.tuyaeu.com'),
+    country('Saint Barthelemy', '590', 'https://openapi.tuyaeu.com'),
+    country('Saint Helena', '290'),
+    country('Saint Kitts and Nevis', '1-869', 'https://openapi.tuyaeu.com'),
+    country('Saint Lucia', '1-758', 'https://openapi.tuyaeu.com'),
+    country('Saint Martin', '590', 'https://openapi.tuyaeu.com'),
+    country('Saint Pierre and Miquelon', '508', 'https://openapi.tuyaeu.com'),
+    country('Saint Vincent and the Grenadines', '1-784', 'https://openapi.tuyaeu.com'),
+    country('Samoa', '685', 'https://openapi.tuyaeu.com'),
+    country('San Marino', '378', 'https://openapi.tuyaeu.com'),
+    country('Sao Tome and Principe', '239', 'https://openapi.tuyaus.com'),
+    country('Saudi Arabia', '966', 'https://openapi.tuyaeu.com'),
+    country('Senegal', '221', 'https://openapi.tuyaeu.com'),
+    country('Serbia', '381', 'https://openapi.tuyaeu.com'),
+    country('Seychelles', '248', 'https://openapi.tuyaeu.com'),
+    country('Sierra Leone', '232', 'https://openapi.tuyaeu.com'),
+    country('Singapore', '65', 'https://openapi.tuyaeu.com'),
+    country('Sint Maarten', '1-721', 'https://openapi.tuyaus.com'),
+    country('Slovakia', '421', 'https://openapi.tuyaeu.com'),
+    country('Slovenia', '386', 'https://openapi.tuyaeu.com'),
+    country('Solomon Islands', '677', 'https://openapi.tuyaus.com'),
+    country('Somalia', '252', 'https://openapi.tuyaeu.com'),
+    country('South Africa', '27', 'https://openapi.tuyaeu.com'),
+    country('South Korea', '82', 'https://openapi.tuyaus.com'),
+    country('South Sudan', '211'),
+    country('Spain', '34', 'https://openapi.tuyaeu.com'),
+    country('Sri Lanka', '94', 'https://openapi.tuyaeu.com'),
+    country('Sudan', '249'),
+    country('Suriname', '597', 'https://openapi.tuyaus.com'),
+    country('Svalbard and Jan Mayen', '4779', 'https://openapi.tuyaus.com'),
+    country('Swaziland', '268', 'https://openapi.tuyaeu.com'),
+    country('Sweden', '46', 'https://openapi.tuyaeu.com'),
+    country('Switzerland', '41', 'https://openapi.tuyaeu.com'),
+    country('Syria', '963'),
+    country('Taiwan', '886', 'https://openapi.tuyaus.com'),
+    country('Tajikistan', '992', 'https://openapi.tuyaeu.com'),
+    country('Tanzania', '255', 'https://openapi.tuyaeu.com'),
+    country('Thailand', '66', 'https://openapi.tuyaus.com'),
+    country('Togo', '228', 'https://openapi.tuyaeu.com'),
+    country('Tokelau', '690', 'https://openapi.tuyaus.com'),
+    country('Tonga', '676', 'https://openapi.tuyaeu.com'),
+    country('Trinidad and Tobago', '1-868', 'https://openapi.tuyaeu.com'),
+    country('Tunisia', '216', 'https://openapi.tuyaeu.com'),
+    country('Turkey', '90', 'https://openapi.tuyaeu.com'),
+    country('Turkmenistan', '993', 'https://openapi.tuyaeu.com'),
+    country('Turks and Caicos Islands', '1-649', 'https://openapi.tuyaeu.com'),
+    country('Tuvalu', '688', 'https://openapi.tuyaeu.com'),
+    country('U.S. Virgin Islands', '1-340', 'https://openapi.tuyaeu.com'),
+    country('Uganda', '256', 'https://openapi.tuyaeu.com'),
+    country('Ukraine', '380', 'https://openapi.tuyaeu.com'),
+    country('United Arab Emirates', '971', 'https://openapi.tuyaeu.com'),
+    country('United Kingdom', '44', 'https://openapi.tuyaeu.com'),
+    country('United States', '1', 'https://openapi.tuyaus.com'),
+    country('Uruguay', '598', 'https://openapi.tuyaus.com'),
+    country('Uzbekistan', '998', 'https://openapi.tuyaeu.com'),
+    country('Vanuatu', '678', 'https://openapi.tuyaus.com'),
+    country('Vatican', '379', 'https://openapi.tuyaeu.com'),
+    country('Venezuela', '58', 'https://openapi.tuyaus.com'),
+    country('Vietnam', '84', 'https://openapi.tuyaus.com'),
+    country('Wallis and Futuna', '681', 'https://openapi.tuyaeu.com'),
+    country('Western Sahara', '212', 'https://openapi.tuyaeu.com'),
+    country('Yemen', '967', 'https://openapi.tuyaeu.com'),
+    country('Zambia', '260', 'https://openapi.tuyaeu.com'),
+    country('Zimbabwe', '263', 'https://openapi.tuyaeu.com')
+]
 
 /**
  *  Driver Capabilities Implementation
@@ -1257,7 +1514,7 @@ private void tuyaRequestAsync(String method, String path, String callback, Map q
       'sign': tuyaCalculateSignature(accessToken, now, stringToSign),
       'sign_method': 'HMAC-SHA256',
       'access_token': accessToken,
-      'lang': 'en'
+      'lang': 'en' // use zh for china
     ]
 
     Map request = [
@@ -1281,13 +1538,18 @@ private void tuyaRequestAsync(String method, String path, String callback, Map q
 }
 
 private boolean tuyaCheckResponse(AsyncResponse response) {
+    if (response.hasError()) {
+        log.error "${device.displayName} cloud request error ${response.getErrorMessage()}"
+        return false
+    }
+
     if (response.status != 200) {
         log.error "${device.displayName} cloud request returned HTTP status ${response.status}"
         return false
     }
 
     if (response.json?.success != true) {
-        log.warn "${device.displayName} cloud request failed: ${response.data}"
+        log.warn "${device.displayName} cloud API request failed: ${response.data}"
         return false
     }
 
