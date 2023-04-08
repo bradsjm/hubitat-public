a2c8b400e32d4e7c134eb99e7ee2aa601e22edf3
Jonathan Bradshaw <jb@nrgup.net>
2023-04-08 11:59:36 -0400

    Code linting and formatting

--- ThirdReality/ThirdRealityPowerMonitorPlug.groovy
+++ ThirdReality/ThirdRealityPowerMonitorPlug.groovy
@@ -14,7 +14,7 @@
  *
  *  THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
  *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
@@ -204,7 +204,7 @@ void updated() {
         runIn(1800, logsOff)
     }
 
-    int interval = (settings.healthCheckInterval as Integer) ?: 0
+    final int interval = (settings.healthCheckInterval as Integer) ?: 0
     if (interval > 0) {
         log.info "scheduling health check every ${interval} minutes"
         scheduleDeviceHealthCheck(interval)
@@ -218,8 +218,8 @@ List<String> updateFirmware() {
     return zigbee.updateFirmware()
 }
 
-void parse(String description) {
-    Map descMap = zigbee.parseDescriptionAsMap(description)
+void parse(final String description) {
+    final Map descMap = zigbee.parseDescriptionAsMap(description)
     updateAttribute('healthStatus', 'online')
     unschedule('deviceCommandTimeout')
 
@@ -235,24 +235,24 @@ void parse(String description) {
     }
 
     if (settings.logEnable) {
-        String clusterName = clusterLookup(descMap.clusterInt)
-        String attribute = descMap.attrId ? " attribute 0x${descMap.attrId} (value ${descMap.value})" : ''
+        final String clusterName = clusterLookup(descMap.clusterInt)
+        final String attribute = descMap.attrId ? " attribute 0x${descMap.attrId} (value ${descMap.value})" : ''
         if (settings.logEnable) { log.trace "zigbee received ${clusterName} message" + attribute }
     }
 
     switch (descMap.clusterInt as Integer) {
         case zigbee.BASIC_CLUSTER:
             parseBasicCluster(descMap)
-            descMap.remove('additionalAttrs')?.each { m -> parseBasicCluster(descMap + m) }
+            descMap.remove('additionalAttrs')?.each { final Map map -> parseBasicCluster(descMap + map) }
             break
         case zigbee.ON_OFF_CLUSTER:
             parseOnOffCluster(descMap)
-            descMap.remove('additionalAttrs')?.each { m -> parseOnOffCluster(descMap + m) }
+            descMap.remove('additionalAttrs')?.each { final Map map -> parseOnOffCluster(descMap + map) }
             break
         case zigbee.ELECTRICAL_MEASUREMENT_CLUSTER:
             if (state.attributes == null) { state.attributes = [:] }
-            parseElectricalMeasurementCluster(descMap)
-            descMap.remove('additionalAttrs')?.each { m -> parseElectricalMeasurementCluster(descMap + m) }
+            parseElectricalMeasureCluster(descMap)
+            descMap.remove('additionalAttrs')?.each { final Map map -> parseElectricalMeasureCluster(descMap + map) }
             break
         default:
             if (settings.logEnable) {
@@ -265,13 +265,13 @@ void parse(String description) {
 /*
  * Zigbee Basic Cluster Parsing
  */
-void parseBasicCluster(Map descMap) {
+void parseBasicCluster(final Map descMap) {
     switch (descMap.attrInt as Integer) {
         case PING_ATTR_ID: // Using 0x01 read as a simple ping/pong mechanism
             if (settings.txtEnable) { log.info 'pong..' }
             break
         case FIRMWARE_VERSION_ID:
-            String version = descMap.value ?: 'unknown'
+            final String version = descMap.value ?: 'unknown'
             log.info "device firmware version is ${version}"
             updateDataValue('softwareBuild', version)
             break
@@ -284,9 +284,9 @@ void parseBasicCluster(Map descMap) {
 /*
  * Zigbee Electrical Measurement Cluster Parsing
  */
-void parseElectricalMeasurementCluster(Map descMap) {
+void parseElectricalMeasureCluster(final Map descMap) {
     if (descMap.value == null || descMap.value == 'FFFF') { return } // invalid or unknown value
-    long value = hexStrToUnsignedInt(descMap.value)
+    final long value = hexStrToUnsignedInt(descMap.value)
     switch (descMap.attrInt as Integer) {
         case AC_CURRENT_DIVISOR_ID:
         case AC_CURRENT_MULTIPLIER_ID:
@@ -300,30 +300,30 @@ void parseElectricalMeasurementCluster(Map descMap) {
             updateAttribute('frequency', value, 'Hz', 'physical')
             break
         case RMS_CURRENT_ID:
-            Integer multiplier = state.attributes[(String)AC_CURRENT_MULTIPLIER_ID]
-            Integer divisor = state.attributes[(String)AC_CURRENT_DIVISOR_ID]
+            final Integer multiplier = state.attributes[AC_CURRENT_MULTIPLIER_ID as String] as Integer
+            final Integer divisor = state.attributes[AC_CURRENT_DIVISOR_ID as String] as Integer
             if (multiplier > 0 && divisor > 0) {
-                BigDecimal result = value * multiplier / divisor
+                final BigDecimal result = value * multiplier / divisor
                 updateAttribute('amperage', result.setScale(1, RoundingMode.HALF_UP), 'A', 'physical')
                 updatePowerFactor()
             }
             break
         case ACTIVE_POWER_ID:
-            Integer multiplier = state.attributes[(String)AC_POWER_MULTIPLIER_ID]
-            Integer divisor = state.attributes[(String)AC_POWER_DIVISOR_ID]
+            final Integer multiplier = state.attributes[AC_POWER_MULTIPLIER_ID as String] as Integer
+            final Integer divisor = state.attributes[AC_POWER_DIVISOR_ID as String] as Integer
             if (multiplier > 0 && divisor > 0) {
                 unschedule('updateEnergyCalculation')
                 updateEnergyCalculation()
-                BigDecimal result = (int)value * multiplier / divisor
+                final BigDecimal result = (int)value * multiplier / divisor
                 updateAttribute('power', result.setScale(1, RoundingMode.HALF_UP), 'W', 'physical')
                 updatePowerFactor()
             }
             break
         case RMS_VOLTAGE_ID:
-            Integer multiplier = state.attributes[(String)AC_VOLTAGE_MULTIPLIER_ID]
-            Integer divisor = state.attributes[(String)AC_VOLTAGE_DIVISOR_ID]
+            final Integer multiplier = state.attributes[AC_VOLTAGE_MULTIPLIER_ID as String] as Integer
+            final Integer divisor = state.attributes[AC_VOLTAGE_DIVISOR_ID as String] as Integer
             if (multiplier > 0 && divisor > 0) {
-                BigDecimal result = value * multiplier / divisor
+                final BigDecimal result = value * multiplier / divisor
                 updateAttribute('voltage', result.setScale(0, RoundingMode.HALF_UP), 'V', 'physical')
                 updatePowerFactor()
             }
@@ -337,11 +337,12 @@ void parseElectricalMeasurementCluster(Map descMap) {
 /*
  * Zigbee Global Command Parsing
  */
-void parseGlobalCommands(Map descMap) {
+void parseGlobalCommands(final Map descMap) {
     switch (hexStrToUnsignedInt(descMap.command)) {
         case 0x04: // write attribute response
-            int statusCode = hexStrToUnsignedInt(descMap.data in List ? descMap.data[0] : descMap.data)
-            String status = "0x${intToHexStr(statusCode)}"
+            final String data = descMap.data in List ? (descMap.data as List)[0] : descMap.data
+            final int statusCode = hexStrToUnsignedInt(data)
+            final String status = "0x${intToHexStr(statusCode)}"
             if (settings.logEnable) {
                 log.trace "zigbee response write ${clusterLookup(descMap.clusterInt)} attribute response: ${status}"
             } else if (statusCode != 0x00) {
@@ -352,9 +353,10 @@ void parseGlobalCommands(Map descMap) {
             log.info "reporting for ${clusterLookup(descMap.clusterInt)} enabled sucessfully"
             break
         case 0x0B: // default command response
-            String commandId = descMap.data[0]
-            int statusCode = hexStrToUnsignedInt(descMap.data[1])
-            String status = "0x${descMap.data[1]}"
+            final List<String> data = descMap.data as List<String>
+            final String commandId = data[0]
+            final int statusCode = hexStrToUnsignedInt(data[1])
+            final String status = "0x${data[1]}"
             if (settings.logEnable) {
                 log.trace "zigbee command status ${clusterLookup(descMap.clusterInt)} command 0x${commandId}: ${status}"
             } else if (statusCode != 0x00) {
@@ -367,16 +369,17 @@ void parseGlobalCommands(Map descMap) {
 /*
  * Zigbee On Off Cluster Parsing
  */
-void parseOnOffCluster(Map descMap) {
+void parseOnOffCluster(final Map descMap) {
     switch (descMap.attrInt as Integer) {
         case POWER_ON_OFF_ID:
-            String type = state.isDigital == true ? 'digital' : 'physical'
+            final String type = state.isDigital == true ? 'digital' : 'physical'
             state.remove('isDigital')
             updateAttribute('switch', descMap.value == '01' ? 'on' : 'off', null, type)
             break
         case POWER_RESTORE_ID:
-            Integer value = hexStrToUnsignedInt(descMap.value)
-            log.info "power restore mode is '${PowerRestoreOpts.options[value]}' (0x${descMap.value})"
+            final Map<Integer, String> options = PowerRestoreOpts.options as Map<Integer, String>
+            final Integer value = hexStrToUnsignedInt(descMap.value)
+            log.info "power restore mode is '${options[value]}' (0x${descMap.value})"
             device.updateSetting('powerRestore', [value: value.toString(), type: 'enum' ])
             break
         default:
@@ -385,7 +388,7 @@ void parseOnOffCluster(Map descMap) {
     }
 }
 
-void parseZdo(Map descMap) {
+void parseZdo(final Map descMap) {
     switch (descMap.clusterInt as Integer) {
         case 0x8005: //endpoint response
             if (settings.logEnable) {
@@ -420,37 +423,37 @@ void parseZdo(Map descMap) {
     }
 }
 
-private static BigDecimal calculateEnergyInKWh(BigDecimal currentPower, Long durationMs) {
-    BigDecimal powerInKw = currentPower / 1000
-    BigDecimal timeInHours = durationMs / (1000 * 60 * 60)
-    BigDecimal energyInKwh = powerInKw * timeInHours
+private static BigDecimal calculateEnergyInKWh(final BigDecimal currentPower, final Long durationMs) {
+    final BigDecimal powerInKw = currentPower / 1000
+    final BigDecimal timeInHours = durationMs / (1000 * 60 * 60)
+    final BigDecimal energyInKwh = powerInKw * timeInHours
     return energyInKwh
 }
 
-private static BigDecimal calculatePowerFactor(BigDecimal rmsVoltage, BigDecimal rmsCurrent, BigDecimal activePower) {
-    BigDecimal apparentPower = rmsVoltage * rmsCurrent
-    BigDecimal powerFactor = activePower / apparentPower
+private static BigDecimal calculatePowerFactor(final BigDecimal rmsVoltage, final BigDecimal rmsCurrent, final BigDecimal activePower) {
+    final BigDecimal apparentPower = rmsVoltage * rmsCurrent
+    final BigDecimal powerFactor = activePower / apparentPower
     return powerFactor
 }
 
-private String clusterLookup(Object cluster) {
-    int clusterInt = cluster in String ? hexStrToUnsignedInt(cluster) : cluster.toInteger()
-    String label = zigbee.clusterLookup(clusterInt)?.clusterLabel
-    String hex = "0x${intToHexStr(clusterInt, 2)}"
+private String clusterLookup(final Object cluster) {
+    final int clusterInt = cluster in String ? hexStrToUnsignedInt(cluster) : cluster.toInteger()
+    final String label = zigbee.clusterLookup(clusterInt)?.clusterLabel
+    final String hex = "0x${intToHexStr(clusterInt, 2)}"
     return label ? "${label} (${hex}) cluster" : "cluster ${hex}"
 }
 
-private void scheduleCommandTimeoutCheck(int delay = COMMAND_TIMEOUT) {
+private void scheduleCommandTimeoutCheck(final int delay = COMMAND_TIMEOUT) {
     runIn(delay, 'deviceCommandTimeout')
 }
 
-private void scheduleDeviceHealthCheck(int intervalMins) {
-    Random rnd = new Random()
+private void scheduleDeviceHealthCheck(final int intervalMins) {
+    final Random rnd = new Random()
     schedule("${rnd.nextInt(59)} ${rnd.nextInt(9)}/${intervalMins} * ? * * *", 'ping')
 }
 
-private void updateAttribute(String attribute, Object value, String unit = null, String type = null) {
-    String descriptionText = "${attribute} was set to ${value}${unit ?: ''}"
+private void updateAttribute(final String attribute, final Object value, final String unit = null, final String type = null) {
+    final String descriptionText = "${attribute} was set to ${value}${unit ?: ''}"
     if (device.currentValue(attribute) != value && settings.txtEnable) {
         log.info descriptionText
     }
@@ -458,13 +461,13 @@ private void updateAttribute(String attribute, Object value, String unit = null,
 }
 
 private void updateEnergyCalculation() {
-    Long now = now()
-    Long lastUpdate = state.lastPowerUpdate as Long ?: now
-    Long elapsedMs = now - lastUpdate
+    final Long now = now()
+    final Long lastUpdate = state.lastPowerUpdate as Long ?: now
+    final Long elapsedMs = now - lastUpdate
     state.lastPowerUpdate = now
-    BigDecimal powerInWatts = device.currentValue('power') as BigDecimal ?: 0
-    BigDecimal energyInKwh = state.energyInKwh as BigDecimal ?: 0
-    BigDecimal result = energyInKwh + calculateEnergyInKWh(powerInWatts, elapsedMs)
+    final BigDecimal powerInWatts = device.currentValue('power') as BigDecimal ?: 0
+    final BigDecimal energyInKwh = state.energyInKwh as BigDecimal ?: 0
+    final BigDecimal result = energyInKwh + calculateEnergyInKWh(powerInWatts, elapsedMs)
     state.energyInKwh = result
     updateAttribute('energy', result.setScale(2, RoundingMode.HALF_UP), 'kWh', 'digital')
     if (powerInWatts > 0) {
@@ -476,11 +479,11 @@ private void updateEnergyCalculation() {
 }
 
 private void updatePowerFactor() {
-    BigDecimal rmsVoltage = device.currentValue('voltage') as BigDecimal
-    BigDecimal rmsCurrent = device.currentValue('amperage') as BigDecimal
-    BigDecimal activePower = device.currentValue('power') as BigDecimal
+    final BigDecimal rmsVoltage = device.currentValue('voltage') as BigDecimal
+    final BigDecimal rmsCurrent = device.currentValue('amperage') as BigDecimal
+    final BigDecimal activePower = device.currentValue('power') as BigDecimal
     if (rmsVoltage && rmsCurrent && activePower) {
-        BigDecimal powerFactor = calculatePowerFactor(rmsVoltage, rmsCurrent, activePower)
+        final BigDecimal powerFactor = calculatePowerFactor(rmsVoltage, rmsCurrent, activePower)
         updateAttribute('powerFactor', powerFactor.setScale(1, RoundingMode.HALF_UP), null, 'digital')
     }
 }
@@ -501,12 +504,12 @@ private void updatePowerFactor() {
 @Field static final int RMS_CURRENT_ID = 0x0508
 @Field static final int RMS_VOLTAGE_ID = 0x0505
 
-@Field static Map PowerRestoreOpts = [
+@Field static final Map PowerRestoreOpts = [
     defaultValue: 0xFF,
     options: [ 0x00: 'Off', 0x01: 'On', 0xFF: 'Last State' ]
 ]
 
-@Field static Map HealthcheckIntervalOpts = [
+@Field static final Map HealthcheckIntervalOpts = [
     defaultValue: 10,
     options: [ 10: 'Every 10 Mins', 15: 'Every 15 Mins', 30: 'Every 30 Mins', 45: 'Every 45 Mins', 59: 'Every Hour', 00: 'Disabled' ]
 ]
@@ -514,5 +517,5 @@ private void updatePowerFactor() {
 // Command timeout before setting healthState to offline
 @Field static final int COMMAND_TIMEOUT = 10
 
-// Delay inbetween zigbee commands
+// Delay in between zigbee commands
 @Field static final int DELAY_MS = 200
